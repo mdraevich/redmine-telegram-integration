9c93bc931175430ac34620abfe8092ead26c6d22
Matthew <draevich.matvey@gmail.com>
2023-02-20 15:37:48 +0300

    2nd commit

--- README.md
+++ README.md
@@ -1,34 +1,82 @@
-# TODO
+# Telegram-Redmine automation for time tracking activity
 
-1. poll_channels_module, handle FloodWaitError exception.
-2. add feature to ignore specific questions from asking.
-3. add feature to add user-defined answers for questions.
-4. add feature to define different parsers for different channel.
-5. add more unit tests before building; add unit tests after building.
+## Motivation
+Time tracking via default Redmine web interface is quite time-consuming and leads to lots of mistakes due to inavailability to edit and delete your issues or log time.
 
-### Create authentication file
+That's a reason why I decided to manage my time tracking activity in smart way via Telegram Channel.
+This project is aimed to sync any data between Telegram Channel and Redmine, so you're free of routine to move data manually.
 
-1. Prepare `./secrets` directory if not exists:
+## Architecture
+Project consists of two Docker images:
+1. **poll_channels** is responsible for interaction with Telegram API in order to export, parse and publish channel posts.
+2. **redmine_pusher** is responsible for interaction with Redmine API in order to import data gathered from **poll_channels** HTTP endpoint.
+
+## Usage
+
+Follow these steps to deploy this solution:
+1. Create Telegram Channel (either private or public one). 
+2. Pull the repository, create environment file and configuration file.
+3. Create authentication file for interacting with Telegram API.
+4. Run `docker compose up -d --build` to get this stuff up & working.
+
+#### Step #1: Telegram Channel message format
+
+Format depends on **poll_channels** configuration, but here's default one:
 ```shell
-# in the project root directory
+1) 1st line has to have two entries as \d\d:\d\d (regex), other characters are ignored
+2) 2-6 lines are parsed as description and parsed as .* (regex)
+3) any other next line is ignored
+```
 
-mkdir -p secrets
+#### Step #2: environment and configuration files
+Generate an environment file that's required by Docker Compose:
+```
+# in the root of project
+
+echo "
+COMPOSE_PROJECT_NAME=redmine-integration
+PYTHONPATH=.
+TZ=Europe/Minsk
+
+# poll channels
+SESSION_NAME=redmine-integration
+LISTEN_ADDRESS=0.0.0.0:8080
+PHONE=<YOUR-TELEGRAM-PHONE-NUMBER>
+API_ID=<API-ID-FOR-TELEGRAM>
+API_HASH=<API-HASH-FOR-TELEGRAM>
+
+
+# redmine pusher
+POLL_CHANNELS_URL=http://telegram-poll-channels:8080/data
+REDMINE_ADDR=<REDMINE-ADDRESS in format http://domain-name.com>
+REDMINE_API_KEY=<REDMINE-ACCESS-API-KEY>
+MAX_DAYS=7
+" > .env
+```
+
+Generate configuration file that's required by **poll_channels** module based on (example.yml)[./example.yml] file and placed it in `./secrets` directory:
+```shell
+# in the root of project
+
+mkdir -p ./secrets
+cp example.yml ./secrets/config.yml
+nano ./secrets/config.yml
 ```
 
 
-2. Prepare environment file with required parameters if not exists:
+#### Step #3: create authentication file
+In order to be working **poll_channels** modules needs Telegram account to access Telegram API. Follow the authentication process to create a session file: 
+
 ```shell
 # in the project root directory
 
-echo "PHONE=$PHONE" >> .env
-echo "API_ID=$API_ID" >> .env
-echo "API_HASH=$API_HASH" >> .env
-```
-
-3. Follow the authentication process to create a session file:
-```shell
-# in the project root directory
-
+mkdir -p ./secrets
 pipenv install
 pipenv run python3 session_handler/main.py
 ```
+
+
+## Q&A
+
+**Q:** What data is **NOT** pushed to Redmine?
+**A:** Data is **NOT** pushed for today; data is **NOT** pushed for the days after **MAX_DAYS** env variable; data is **NOT** pushed for the days which equal to `start_date` value for any of the last 15 issues. Other data is pushed to a newly created issue with `start_date` set to the published day of data in your Telegram Channel.
---
+++ listen_and_repeat_bot/redmine_pusher/main.py
@@ -0,0 +1,214 @@
+import os
+import time
+import json
+import datetime
+import logging
+from urllib import request, error
+from redminelib import Redmine
+
+
+LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=LOGLEVEL
+)
+logger = logging.getLogger(__name__)
+
+
+def get_channel_data():
+    """
+    gathers JSON file from HTTP endpoint containing 
+    posts for all configured channels
+    """
+    try:
+        contents = request.urlopen(poll_channel_url).read()
+        data = json.loads(contents)
+    except (ConnectionError, error.URLError) as exc:
+        return (False, None)
+    return (True, data)
+
+
+def process_channel_data_element(element):
+    """
+    takes a single element of channel data object and transforms it 
+    """
+
+    if "timestamp" not in element or "matches" not in element:
+        return None
+    if len(element["matches"][0]) < 2:
+        return None
+
+    # TODO, needs to be handled by try-except block
+    from_timestamp = datetime.datetime(2023,1,1,
+                        hour=int(element["matches"][0][0].split(":")[0]),
+                        minute=int(element["matches"][0][0].split(":")[1]))
+    to_timestamp = datetime.datetime(2023,1,1,
+                        hour=int(element["matches"][0][1].split(":")[0]),
+                        minute=int(element["matches"][0][1].split(":")[1]))
+
+    if from_timestamp > to_timestamp:
+        # to_timestamp should be within the same day,
+        # but it's related to the next day, so change day number
+        to_timestamp = datetime.datetime(2023,1,2,
+                        hour=int(element["matches"][0][1].split(":")[0]),
+                        minute=int(element["matches"][0][1].split(":")[1]))
+
+    formatted_element = {
+        "message_timestamp": datetime.datetime.fromtimestamp(element["timestamp"]),
+        "job_start": element["matches"][0][0],
+        "job_stop": element["matches"][0][1],
+        "job_amount": (to_timestamp - from_timestamp).total_seconds() / 60 / 60,
+        "job_description": " ".join(
+                        list(map(" ".join, element["matches"][1:]))).strip()
+    } 
+    return formatted_element
+
+def process_channel_data(data):
+    """
+    takes channel data object and transforms it 
+    """
+
+    formatted_data = list(map(process_channel_data_element, data))
+    return formatted_data
+
+
+def get_issues(redmine_address, project_id, limit, **kwargs):
+    """
+    returns issues
+    """ 
+    redmine = Redmine(redmine_address,
+                      key=kwargs["redmine_api_key"])
+
+    return redmine.issue.filter(project_id=project_id,
+                                status_id="*",
+                                limit=limit)
+
+
+def create_issue(redmine_address, project_id, subject, **kwargs):
+    """
+    creates issue in redmine via REST API
+    """
+    redmine = Redmine(redmine_address,
+                      key=kwargs["redmine_api_key"])
+    return redmine.issue.create(
+        project_id=project_id,
+        subject=subject,
+        tracker_id=kwargs["tracker_id"],
+        status_id=kwargs["status_id"],
+        priority_id=kwargs["priority_id"],
+        start_date=kwargs["start_date"],
+        assigned_to_id=kwargs["assigned_to_id"],
+        estimated_hours=kwargs["estimated_hours"]
+    )
+
+
+def log_time_in_issue(redmine_address, issue_id, hours, **kwargs):
+    """
+    log time within the specified issue id
+    """
+    redmine = Redmine(redmine_address,
+                      key=kwargs["redmine_api_key"])
+    return redmine.time_entry.create(
+        issue_id=issue_id,
+        spent_on=kwargs["start_date"],
+        hours=hours,
+        activity_id=kwargs["activity_id"],
+        user_id=kwargs["user_id"],
+        comments=kwargs["description"]
+    )
+
+
+if __name__ == '__main__':
+    poll_channel_url = os.environ.get("POLL_CHANNELS_URL", None)
+    redmine_address = os.environ.get("REDMINE_ADDR", "")
+    redmine_username = os.environ.get("REDMINE_USERNAME", "")
+    redmine_password = os.environ.get("REDMINE_PASSWORD", "")
+    redmine_api_key = os.environ.get("REDMINE_API_KEY", "")
+    max_days = int(os.environ.get("MAX_DAYS", "7"))
+
+    while True:
+        success, data_obj = get_channel_data()
+        if success:
+            logger.info("Success to retrieve data, try to format it")
+
+            for channel in data_obj["channels"]:
+                logger.debug(process_channel_data(channel["data"]))
+                
+                created_issues = get_issues(redmine_address,
+                                    channel["tags"]["project_id"],
+                                    limit=15,
+                                    redmine_api_key=redmine_api_key)
+                logger.debug([ i.subject for i in created_issues ])
+
+                # dict of lists,
+                # key = issue.start_day
+                # value = list() of time events
+                events_to_push = {}
+                today = datetime.datetime.today()
+                for element in process_channel_data(channel["data"]):
+                    if element is None:
+                        # element was parsed incorrectly
+                        logger.warning("Channel data element was parsed "
+                                       "incorrectly, skip it") 
+                        continue
+
+                    el_datetime = element["message_timestamp"]
+                    el_date = el_datetime.date()
+                    if el_datetime.day == today.day:
+                        # skip pushing time events for today
+                        logger.info("%s: skip pushing events for today",
+                                    el_date.strftime("%d.%m.%Y"))
+                        continue
+                    if (today.date() - el_date).days > max_days:
+                        logger.info("%s: skip outdated events (MAX_DAYS=%s)",
+                                    el_date.strftime("%d.%m.%Y"),
+                                    max_days)
+                        # skip pushing outdated time events
+                        continue
+
+                    is_issue_created = any(
+                        [i.start_date == el_date for i in created_issues])
+                    logger.info("%s: is_issue_created=%s",
+                                   el_date.strftime("%d.%m.%Y"),
+                                   is_issue_created)
+
+                    if is_issue_created: 
+                        # we're not going to recreate issue
+                        # neither pushing time events to it
+                        continue
+
+                    # if issue is not already created 
+                    # and it's not today and it's not outdated event,
+                    # then add event to push later outside of for-cycle
+                    key = el_date.strftime("%d.%m.%Y")
+                    if key not in events_to_push:
+                        events_to_push[key] = []
+                    events_to_push[key].append(element)
+
+                logger.debug(events_to_push)
+                for key, value in events_to_push.items():
+                    key_date = datetime.datetime.strptime(key, "%d.%m.%Y").date()
+                    issue = create_issue(
+                                redmine_address,
+                                channel["tags"]["project_id"],
+                                "{}: automated by redmine-pusher".format(key),
+                                redmine_api_key=redmine_api_key,
+                                tracker_id=channel["tags"]["tracker_id"],
+                                status_id=channel["tags"]["status_id"],
+                                priority_id=channel["tags"]["priority_id"],
+                                start_date=key_date,
+                                assigned_to_id=channel["tags"]["user_id"],
+                                estimated_hours=8)
+
+                    for event in value:
+                        log_time_in_issue(
+                                redmine_address,
+                                issue_id=issue.id,
+                                hours=event["job_amount"],
+                                redmine_api_key=redmine_api_key,
+                                start_date=key_date,
+                                activity_id=channel["tags"]["activity_id"],
+                                user_id=channel["tags"]["user_id"],
+                                description=event["job_description"])
+        else:
+            logger.error("Failed to retrieve data")
+        time.sleep(60 * 15)
